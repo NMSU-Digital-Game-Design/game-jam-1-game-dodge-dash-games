14bd69b3e9c6a9a84e8a4e2dcd694ccca3480a32
WenYangNMSU <55725222+WenYangNMSU@users.noreply.github.com>
2025-10-03 18:49:04 -0600

    Background and camera update

--- arena-boss-clash/test/camera.gd
+++ arena-boss-clash/test/camera.gd
@@ -1,12 +1,11 @@
 extends Camera2D
 
 func _physics_process(delta: float) -> void:
-	print(position)
 	if Input.is_action_pressed("ui_left"):
-		offset.x -= 10
+		position.x -= 10
 	if Input.is_action_pressed("ui_right"):
-		offset.x += 10
+		position.x += 10
 	if Input.is_action_pressed("ui_up"):
-		offset.y -= 10
+		position.y -= 10
 	if Input.is_action_pressed("ui_down"):
-		offset.y += 10
+		position.y += 10
